c417f3534959c39b3f995c10ae9f012c56e04cdf
Đức Nghĩa <nghiank1999@gmail.com>
2025-04-30 23:23:04 +0700

    update

--- app/observers/singleton.py
+++ app/observers/singleton.py
@@ -1,7 +1,12 @@
-class SingletonMeta(type):
-    _instances = {}
+from app.observers.subject import Observer
+
+ObserverMeta = type(Observer)
+
+class SingletonMeta(ObserverMeta):
+    _instances: dict[type, object] = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
+            ObserverMeta.__call__(cls, *args, **kwargs)
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
     
